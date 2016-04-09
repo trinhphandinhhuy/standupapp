7902e6b3d00167e5171c7d671dc8ee6517a209c5
huytrinhtest <contact@huytrinh.in>
2016-04-09 10:18:45 +0300

    first version of the app

--- controllers/standup.server.controller.js
+++ controllers/standup.server.controller.js
@@ -1,11 +1,51 @@
 var Standup = require('../models/standup.server.model.js');
+exports.list = function (req, res) {
+  var query = Standup.find();
+
+  query.sort({createdOn: 'desc'})
+  .limit(12)
+  .exec(function (err, results) {
+    res.render('index', {title: 'Standup - List', notes: results});
+  });
+};
 
 exports.create = function (req, res) {
   var entry = new Standup({
     memberName : req.body.memberName,
     project: req.body.project,
     workYesterday: req.body.workYesterday,
-    wordToday: req.body.workToday,
+    workToday: req.body.workToday,
     impediment: req.body.impediment
+  });
+
+
+
+  entry.save(function (err) {
+    if(err){
+      var errMsg = 'Sorry, there was an error saving the note.' + err;
+      res.render('newnote', {title: 'Standup - New note (error)', message: errMsg});
+    } else {
+      console.log('The note was saved!');
+      res.redirect(301, '/');
+    }
+  });
+};
+
+exports.getNote = function (req, res) {
+  res.render('newnote', {title : 'Standup - New note'});
+};
+
+exports.filterByMember = function (req, res) {
+  var query = Standup.find();
+  var filter = req.body.memberName;
+
+  query.sort({createdOn : 'desc'});
+
+  if(filter.length > 0){
+    query.where({memberName: filter});
+  }
+
+  query.exec(function (err, results) {
+    res.render('index', {title: 'Standup - List', notes: results});
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var standupCtrl = require('../controllers/standup.server.controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return standupCtrl.list(req, res);
+  next();
+});
+
+router.post('/', function (req, res) {
+  return standupCtrl.filterByMember(req, res);
+})
+
+router.get('/newnote', function (req, res) {
+  return standupCtrl.getNote(req, res);
+});
+
+router.post('/newnote', function (req, res) {
+  return standupCtrl.create(req, res);
 });
 
 module.exports = router;
